5190f8d743ad009a6c47722f785919b25db31950
kunal <kunalshelke123@gmail.com>
2021-08-06 07:11:40 +0530

    initial commit

---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,58 @@
+import React,{useState} from 'react'
+//Libraries
+import {Container,FormGroup,Input, Button,Form, InputGroup,InputGroupAddon} from "reactstrap"
+import {v4} from "uuid";
+
+//redux
+import {connect} from "react-redux";
+import {addTodo} from "../actions/todo"
+
+const TodoForm = ({addTodo}) => {
+
+    const [title,setTitle] = useState("");
+
+    const handleSubmit = () => {
+        e.preventDefault();
+        if(title ===''){
+            return alert('Plese add a todo')
+        }
+
+        const todo = {
+            title,
+            id: v4()
+        }
+
+        addTodo(todo);
+
+        setTitle("");
+    }
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <FormGroup>
+                <InputGroup>
+                <Input
+                type="text"
+                name="todo"
+                id="todo"
+                placeholder="Your next Todo"
+                value={title}
+                onChange={e=>setTitle(e.target.value)}
+                />
+                <InputGroupAddon addonType="prepend">
+                    <Button color="primary" onClick={handleSubmit}>ADD</Button>
+                </InputGroupAddon>
+                </InputGroup>
+            </FormGroup>
+        </Form>
+    )
+}
+
+const mapStateToProps = state => ({})
+
+const mapDispatchToProps = dispatch => ({
+    addTodo: todo => {
+        dispatch(addTodo(todo))
+    }
+})
+export default connect(mapStateToProps,mapDispatchToProps)(TodoForm)
